df937574ea64e99892b19f38c671a0c81cb943c6
grateful-library-0184 <alexcouts18@gmail.com>
2019-02-26 22:16:29 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -16,13 +16,10 @@ function logWhisper(string){
 
 function sayHiToGrandma(string){
   var uppercase = "HELLO"
-  if (uppercase.toUpperCase() === uppercase){
+  if (uppercase.toUpperCase() === uppercase)
     return ("YES INDEED")
   } 
-  var lowercase = "hello"
-  if (lowercase.toLowerCase() === lowercase)
-    return("I can't hear you")
-} 
+ 
   
   
   
