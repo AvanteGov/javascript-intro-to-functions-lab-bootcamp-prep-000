7dec7f4e53995e0b39f9652e7be133f1d6bafe84
grateful-library-0184 <alexcouts18@gmail.com>
2019-02-26 21:50:05 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,11 +6,10 @@ function whisper(string){
   return string.toLowerCase()
 }
 
-var uppercase = "HELLO!"
- 
-uppercase.toUpperCase() === uppercase
-
 function logShout(string) {
   console.log(`${string}`)
 }
 
+var uppercase = "HELLO!"
+ 
+uppercase.toUpperCase() === uppercase
